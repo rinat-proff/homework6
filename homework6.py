a465dbdf1062bfe7246d6a8ff479f598fcb9f621
rinat-proff <rinat-proff@mail.ru>
2024-06-03 20:54:13 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_dict = {"Rinat" : 1986, "Daniya" : 1979}
+print(my_dict)
+print(my_dict["Daniya"])
+my_dict["Venera"] = 1983
+print(my_dict)
+my_dict.update({"Dasha" : 1987,
+"Mariya" : 1988})
+print(my_dict)
+del my_dict["Mariya"]
+print(my_dict)
+my_set_ = {1, 3, 5, 7, 9, 1, 3, 7, 9, 6}
+print(my_set_)
+print(my_set_.discard(1))
+print(my_set_)
